3c8ffb2507910eb6b88abc9a3c3aecb5c72ac21a
geonryulPark <geonryul0131@gmail.com>
2022-11-18 23:09:32 +0900

    trans edit-delete

--- cachelab-handout/trans.c
+++ cachelab-handout/trans.c
@@ -21,15 +21,15 @@ int is_transpose(int M, int N, int A[N][M], int B[M][N]);
  */
 char transpose_submit_desc[] = "Transpose submission";
 void transpose_submit(int M, int N, int A[N][M], int B[M][N]) {
-    int blockForRow, blockForCol; 
+    int rowBlock, colBlock; 
     int row, col;
-    int v0, v1, v2, v3, v4, v5, v6, v7;
+    int v0, v1;
 
     if (N == 32) {
-        for (blockForCol = 0; blockForCol < M; blockForCol += 8) {
-            for (blockForRow = 0; blockForRow < N; blockForRow += 8) {
-                for (row = blockForRow; row < blockForRow + 8; row++) {
-                    for (col = blockForCol; col < blockForCol + 8; col++) {
+        for (colBlock = 0; colBlock < M; colBlock += 8) {
+            for (rowBlock = 0; rowBlock < N; rowBlock += 8) {
+                for (row = rowBlock; row < rowBlock + 8; row++) {
+                    for (col = colBlock; col < colBlock + 8; col++) {
                         if (row != col) {
                             B[col][row] = A[row][col];
                         } else {
@@ -37,72 +37,17 @@ void transpose_submit(int M, int N, int A[N][M], int B[M][N]) {
                             v1 = row;
                         }
                     }
-                    if (blockForRow == blockForCol) {
+                    if (rowBlock == colBlock) {
                         B[v1][v1] = v0;
                     }
                 }
             }
         }
-    } else if (N == 64) {
-        for (blockForCol = 0; blockForCol < M; blockForCol += 8) {
-            for (blockForRow = 0; blockForRow < N; blockForRow += 8) {
-                for (row = blockForRow; row < blockForRow + 4; row++) {
-                    v0 = A[row][blockForCol];
-                    v1 = A[row][blockForCol + 1];
-                    v2 = A[row][blockForCol + 2];
-                    v3 = A[row][blockForCol + 3];
-                    v4 = A[row][blockForCol + 4];
-                    v5 = A[row][blockForCol + 5];
-                    v6 = A[row][blockForCol + 6];
-                    v7 = A[row][blockForCol + 7];
-                    B[blockForCol][row] = v0;
-                    B[blockForCol + 1][row] = v1;
-                    B[blockForCol + 2][row] = v2;
-                    B[blockForCol + 3][row] = v3;
-                    B[blockForCol][row + 4] = v4;
-                    B[blockForCol + 1][row + 4] = v5;
-                    B[blockForCol + 2][row + 4] = v6;
-                    B[blockForCol + 3][row + 4] = v7;
-                }
-                for (col = blockForCol; col < blockForCol + 4; col++) {
-
-                    // A left-down
-                    v4 = A[blockForRow + 4][col];
-                    v5 = A[blockForRow + 5][col];
-                    v6 = A[blockForRow + 6][col];
-                    v7 = A[blockForRow + 7][col];
-
-                    // B right-above
-                    v0 = B[col][blockForRow + 4];
-                    v1 = B[col][blockForRow + 5];
-                    v2 = B[col][blockForRow + 6];
-                    v3 = B[col][blockForRow + 7];
-
-                    // set B right-above
-                    B[col][blockForRow + 4] = v4;
-                    B[col][blockForRow + 5] = v5;
-                    B[col][blockForRow + 6] = v6;
-                    B[col][blockForRow + 7] = v7;
-
-                    // set B left-down
-                    B[col + 4][blockForRow] = v0;
-                    B[col + 4][blockForRow + 1] = v1;
-                    B[col + 4][blockForRow + 2] = v2;
-                    B[col + 4][blockForRow + 3] = v3;
-
-                    // set B right-down
-                    B[col + 4][blockForRow + 4] = A[blockForRow + 4][col + 4];
-                    B[col + 4][blockForRow + 5] = A[blockForRow + 5][col + 4];
-                    B[col + 4][blockForRow + 6] = A[blockForRow + 6][col + 4];
-                    B[col + 4][blockForRow + 7] = A[blockForRow + 7][col + 4];
-                }
-            }
-        }
     } else {
-        for (blockForCol = 0; blockForCol < M; blockForCol += 16) {
-            for (blockForRow = 0; blockForRow < N; blockForRow += 16) {
-                for (row = blockForRow; (row < N) && (row < blockForRow + 16); row++) {
-                    for (col = blockForCol; (col < M) && (col < blockForCol + 16); col++) {
+        for (colBlock = 0; colBlock < M; colBlock += 16) {
+            for (rowBlock = 0; rowBlock < N; rowBlock += 16) {
+                for (row = rowBlock; (row < N) && (row < rowBlock + 16); row++) {
+                    for (col = colBlock; (col < M) && (col < colBlock + 16); col++) {
                         if (row != col) {
                             B[col][row] = A[row][col];
                         } else {
@@ -110,7 +55,7 @@ void transpose_submit(int M, int N, int A[N][M], int B[M][N]) {
                             v1 = row;
                         }
                     }
-                    if (blockForRow == blockForCol) {
+                    if (rowBlock == colBlock) {
                         B[v1][v1] = v0;
                     }
                 }
