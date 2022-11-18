7eeef160f8620f6e8118753e1154652f2d3f8acc
geonryulPark <geonryul0131@gmail.com>
2022-11-18 10:56:18 +0900

    csim fin

--- cachelab-handout/csim.c
+++ cachelab-handout/csim.c
@@ -1,8 +1,177 @@
-// 2019040564 박건률
 #include "cachelab.h"
+#include <stdio.h>
+#include <getopt.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdbool.h>
+#include <limits.h>
+#include <math.h>
 
-int main()
-{
-    printSummary(0, 0, 0);
+typedef unsigned long long int mem_add;
+typedef struct cache_line_t {
+    bool valid;
+    unsigned long long int lru;
+    mem_add tag;
+
+} cache_line_t;
+typedef cache_line_t *cache_set_t;
+typedef cache_set_t *cache_t;
+
+int s = 0, S;
+int b = 0, B;
+int E = 0;
+int verbose;
+int setIndexMask;
+int lruCounter = 0;
+
+FILE *trace = NULL;
+char *trace_file_name = NULL;
+cache_t cache;
+
+unsigned int hit = 0;
+unsigned int miss = 0;
+unsigned int evict = 0;
+
+void setParameter(int argc, char *argv[]); // s, E, b setting
+void accessCache(mem_add address, char command, int blockSize);
+void initSimulate(void);
+
+int main(int argc, char *argv[]) {
+    setParameter(argc, argv);
+    initSimulate();
+    printSummary(hit, miss, evict);
     return 0;
+}
+
+void setParameter(int argc, char *argv[]) {
+    char c;
+
+    while ((c = getopt(argc, argv, "vh:s:E:b:t:")) != -1) {
+
+        switch (c) {
+        case 'v': verbose = 1; break;
+        case 's': s = atoi(optarg); break;
+        case 'E': E = atoi(optarg); break;
+        case 'b': b = atoi(optarg); break;
+        case 't': trace_file_name = optarg; break;
+        default: break;
+        }
+    }
+
+
+//    1111
+    setIndexMask = (1 << s) - 1;
+
+//    10000
+    S = (1 << s);
+    B = (1 << b);
+
+    cache = (cache_t) malloc(sizeof(cache_set_t) * S);
+    for (int i = 0; i < S; i++) {
+        cache[i] = (cache_set_t) malloc(sizeof(cache_line_t) * E);
+        for (int j = 0; j < E; j++) {
+            cache[i][j].valid = false;
+            cache[i][j].tag = 0;
+            cache[i][j].lru = 0;
+        }
+    }
+}
+
+void initSimulate(void) {
+    trace = fopen(trace_file_name, "r");
+    char command;
+    mem_add address;
+    int blockSize;
+    while (fscanf(trace, " %c %llx,%d", &command, &address, &blockSize) != EOF) {
+        switch (command) {
+            case 'L':
+                accessCache(address, command, blockSize);
+                break;
+            case 'M':
+                accessCache(address, command, blockSize);
+                accessCache(address, command, blockSize);
+                break;
+            case 'S':
+                accessCache(address, command, blockSize);
+                break;
+            default:
+                break;
+        }
+    }
+
+    fclose(trace);
+}
+
+void accessCache(mem_add address, char command, int blockSize) {
+    if (verbose == 0) {
+        mem_add setIndex = (address >> b) & setIndexMask;
+        mem_add tag = address >> (s + b);
+
+        for (int i = 0; i < E; i++) {
+            if (cache[setIndex][i].valid) {
+                if (cache[setIndex][i].tag == tag) {
+                    cache[setIndex][i].lru = lruCounter++;
+                    hit++;
+                    return;
+                }
+            }
+        }
+        // 아래부터는 miss 발생한 로직
+        miss++;
+
+        unsigned long long int minLRU = LONG_MAX;
+        int replaceLine;
+
+        for (int j = 0; j < E; j++) {
+            if (cache[setIndex][j].lru < minLRU) {
+                replaceLine = j;
+                minLRU = cache[setIndex][j].lru;
+            }
+        }
+
+        if (cache[setIndex][replaceLine].valid) {
+            evict++;
+        }
+
+        cache[setIndex][replaceLine].valid = true;
+        cache[setIndex][replaceLine].tag = tag;
+        cache[setIndex][replaceLine].lru = lruCounter++;
+    } else {
+        mem_add setIndex = (address >> b) & setIndexMask;
+        mem_add tag = address >> (s + b);
+
+        for (int i = 0; i < E; i++) {
+            if (cache[setIndex][i].valid) {
+                if (cache[setIndex][i].tag == tag) {
+                    cache[setIndex][i].lru = lruCounter++;
+                    hit++;
+                    printf("%c %llx, %d hit\n", command, address, blockSize);
+                    return;
+                }
+            }
+        }
+        // 아래부터는 miss 발생한 로직
+        miss++;
+        printf("%c %llx, %d miss\n", command, address, blockSize);
+
+        unsigned long long int minLRU = LONG_MAX;
+        int replaceLine;
+
+        for (int j = 0; j < E; j++) {
+            if (cache[setIndex][j].lru < minLRU) {
+                replaceLine = j;
+                minLRU = cache[setIndex][j].lru;
+            }
+        }
+
+        if (cache[setIndex][replaceLine].valid) {
+            printf("%c %llx, %d eviction\n", command, address, blockSize);
+            evict++;
+        }
+
+        cache[setIndex][replaceLine].valid = true;
+        cache[setIndex][replaceLine].tag = tag;
+        cache[setIndex][replaceLine].lru = lruCounter++;
+    }
+
 }
--- cachelab-handout/trans.c
+++ cachelab-handout/trans.c
@@ -20,10 +20,104 @@ int is_transpose(int M, int N, int A[N][M], int B[M][N]);
  *     be graded. 
  */
 char transpose_submit_desc[] = "Transpose submission";
-void transpose_submit(int M, int N, int A[N][M], int B[M][N])
-{
-}
+void transpose_submit(int M, int N, int A[N][M], int B[M][N]) {
+    int blockForRow, blockForCol; 
+    int row, col;
+    int v0, v1, v2, v3, v4, v5, v6, v7;
 
+    if (N == 32) {
+        for (blockForCol = 0; blockForCol < M; blockForCol += 8) {
+            for (blockForRow = 0; blockForRow < N; blockForRow += 8) {
+                for (row = blockForRow; row < blockForRow + 8; row++) {
+                    for (col = blockForCol; col < blockForCol + 8; col++) {
+                        if (row != col) {
+                            B[col][row] = A[row][col];
+                        } else {
+                            v0 = A[row][col];
+                            v1 = row;
+                        }
+                    }
+                    if (blockForRow == blockForCol) {
+                        B[v1][v1] = v0;
+                    }
+                }
+            }
+        }
+    } else if (N == 64) {
+        for (blockForCol = 0; blockForCol < M; blockForCol += 8) {
+            for (blockForRow = 0; blockForRow < N; blockForRow += 8) {
+                for (row = blockForRow; row < blockForRow + 4; row++) {
+                    v0 = A[row][blockForCol];
+                    v1 = A[row][blockForCol + 1];
+                    v2 = A[row][blockForCol + 2];
+                    v3 = A[row][blockForCol + 3];
+                    v4 = A[row][blockForCol + 4];
+                    v5 = A[row][blockForCol + 5];
+                    v6 = A[row][blockForCol + 6];
+                    v7 = A[row][blockForCol + 7];
+                    B[blockForCol][row] = v0;
+                    B[blockForCol + 1][row] = v1;
+                    B[blockForCol + 2][row] = v2;
+                    B[blockForCol + 3][row] = v3;
+                    B[blockForCol][row + 4] = v4;
+                    B[blockForCol + 1][row + 4] = v5;
+                    B[blockForCol + 2][row + 4] = v6;
+                    B[blockForCol + 3][row + 4] = v7;
+                }
+                for (col = blockForCol; col < blockForCol + 4; col++) {
+
+                    // A left-down
+                    v4 = A[blockForRow + 4][col];
+                    v5 = A[blockForRow + 5][col];
+                    v6 = A[blockForRow + 6][col];
+                    v7 = A[blockForRow + 7][col];
+
+                    // B right-above
+                    v0 = B[col][blockForRow + 4];
+                    v1 = B[col][blockForRow + 5];
+                    v2 = B[col][blockForRow + 6];
+                    v3 = B[col][blockForRow + 7];
+
+                    // set B right-above
+                    B[col][blockForRow + 4] = v4;
+                    B[col][blockForRow + 5] = v5;
+                    B[col][blockForRow + 6] = v6;
+                    B[col][blockForRow + 7] = v7;
+
+                    // set B left-down
+                    B[col + 4][blockForRow] = v0;
+                    B[col + 4][blockForRow + 1] = v1;
+                    B[col + 4][blockForRow + 2] = v2;
+                    B[col + 4][blockForRow + 3] = v3;
+
+                    // set B right-down
+                    B[col + 4][blockForRow + 4] = A[blockForRow + 4][col + 4];
+                    B[col + 4][blockForRow + 5] = A[blockForRow + 5][col + 4];
+                    B[col + 4][blockForRow + 6] = A[blockForRow + 6][col + 4];
+                    B[col + 4][blockForRow + 7] = A[blockForRow + 7][col + 4];
+                }
+            }
+        }
+    } else {
+        for (blockForCol = 0; blockForCol < M; blockForCol += 16) {
+            for (blockForRow = 0; blockForRow < N; blockForRow += 16) {
+                for (row = blockForRow; (row < N) && (row < blockForRow + 16); row++) {
+                    for (col = blockForCol; (col < M) && (col < blockForCol + 16); col++) {
+                        if (row != col) {
+                            B[col][row] = A[row][col];
+                        } else {
+                            v0 = A[row][col];
+                            v1 = row;
+                        }
+                    }
+                    if (blockForRow == blockForCol) {
+                        B[v1][v1] = v0;
+                    }
+                }
+            }
+        }
+    }
+}
 /* 
  * You can define additional transpose functions below. We've defined
  * a simple one below to help you get started. 
